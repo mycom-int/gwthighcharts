607cb56d560c94448eca7389d7afd7d385c1e820
Ronan Quillevere <ronan.quillevere@gmail.com>
2013-11-07 13:52:08 +0100

    Fix with scheduler

--- gwtapp/src/com/mycom/gwthighcharts/client/Gwthighcharts.java
+++ gwtapp/src/com/mycom/gwthighcharts/client/Gwthighcharts.java
@@ -10,8 +10,11 @@ import org.moxieapps.gwt.highcharts.client.ToolTipData;
 import org.moxieapps.gwt.highcharts.client.ToolTipFormatter;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimpleLayoutPanel;
@@ -54,12 +57,12 @@ public class Gwthighcharts implements EntryPoint
 
    public void startInDockLayoutPanel()
    {
-      RootLayoutPanel rootPanel = RootLayoutPanel.get();
-      Chart chart = createChart();
+      final RootLayoutPanel rootPanel = RootLayoutPanel.get();
+      final Chart chart = createChart();
       chart.setWidth100();
       chart.setHeight100();
 
-      DockLayoutPanel dock1 = new DockLayoutPanel(Unit.PX);
+      final DockLayoutPanel dock1 = new DockLayoutPanel(Unit.PX);
 
       SimpleLayoutPanel slp1 = new SimpleLayoutPanel();
       slp1.getElement().getStyle().setBackgroundColor("blue");
@@ -67,25 +70,61 @@ public class Gwthighcharts implements EntryPoint
       SimpleLayoutPanel slp2 = new SimpleLayoutPanel();
       slp2.getElement().getStyle().setBackgroundColor("yellow");
 
-      SimpleLayoutPanel slp3 = new SimpleLayoutPanel();
-      slp3.getElement().getStyle().setBackgroundColor("red");
-
-      SimpleLayoutPanel slp4 = new SimpleLayoutPanel();
-      slp4.getElement().getStyle().setBackgroundColor("green");
-
       dock1.addNorth(slp1, 50);
-      // dock1.addSouth(slp3, 50);
       dock1.addWest(slp2, 50);
-      // dock1.addEast(slp4, 50);
+
 
       dock1.add(chart);
       rootPanel.add(dock1);
+
+      Scheduler scheduler = Scheduler.get();
+      scheduler.scheduleDeferred(new ScheduledCommand()
+      {
+
+         @Override
+         public void execute()
+         {
+            chart.setSizeToMatchContainer();
+         }
+      });
+   }
+
+
+   public void startInLayoutPanels()
+   {
+      RootLayoutPanel rootPanel = RootLayoutPanel.get();
+
+      final Chart chart = createChart();
+      chart.setWidth100();
+      chart.setHeight100();
+
+      LayoutPanel container = new LayoutPanel();
+
+      LayoutPanel north = new LayoutPanel();
+      north.getElement().getStyle().setBackgroundColor("blue");
+      LayoutPanel west = new LayoutPanel();
+      west.getElement().getStyle().setBackgroundColor("red");
+
+
+      container.add(north);
+      container.setWidgetTopHeight(north, 0, Unit.PX, 25, Unit.PX);
+
+      container.add(west);
+      container.setWidgetLeftWidth(west, 0, Unit.PX, 25, Unit.PX);
+
+
+      container.add(chart);
+      container.setWidgetTopBottom(chart, 25, Unit.PX, 0, Unit.PX);
+      container.setWidgetLeftRight(chart, 25, Unit.PX, 0, Unit.PX);
+
+      rootPanel.add(container);
    }
 
    public Chart createChart()
    {
 
-      final Chart chart = new Chart().setType(Series.Type.LINE)
+      final Chart chart = new Chart();
+      chart.setType(Series.Type.LINE)
          .setMarginRight(130)
          .setMarginBottom(25)
          .setChartTitle(new ChartTitle().setText("Monthly Average Temperature").setX(-20) // center
@@ -112,8 +151,22 @@ public class Gwthighcharts implements EntryPoint
       chart.getYAxis().setAxisTitleText("Temperature °C");
 
       chart.addSeries(chart.createSeries().setName("Tokyo").setPoints(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 }));
+      chart.addSeries(chart.createSeries().setName("Tokyo").setPoints(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 }));
+      chart.addSeries(chart.createSeries().setName("Tokyo").setPoints(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 }));
+      chart.addSeries(chart.createSeries().setName("Tokyo").setPoints(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 }));
+      chart.addSeries(chart.createSeries().setName("Tokyo").setPoints(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 }));
+      chart.addSeries(chart.createSeries().setName("Tokyo").setPoints(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 }));
+      chart.addSeries(chart.createSeries().setName("Tokyo").setPoints(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 }));
+      chart.addSeries(chart.createSeries().setName("Tokyo").setPoints(new Number[] { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 }));
       chart.addSeries(chart.createSeries().setName("New York").setPoints(new Number[] { -0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5 }));
       chart.addSeries(chart.createSeries().setName("Berlin").setPoints(new Number[] { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 }));
+      chart.addSeries(chart.createSeries().setName("Berlin").setPoints(new Number[] { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 }));
+      chart.addSeries(chart.createSeries().setName("Berlin").setPoints(new Number[] { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 }));
+      chart.addSeries(chart.createSeries().setName("Berlin").setPoints(new Number[] { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 }));
+      chart.addSeries(chart.createSeries().setName("Berlin").setPoints(new Number[] { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 }));
+      chart.addSeries(chart.createSeries().setName("Berlin").setPoints(new Number[] { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 }));
+      chart.addSeries(chart.createSeries().setName("Berlin").setPoints(new Number[] { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 }));
+
       chart.addSeries(chart.createSeries().setName("London").setPoints(new Number[] { 3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8 }));
 
       return chart;
